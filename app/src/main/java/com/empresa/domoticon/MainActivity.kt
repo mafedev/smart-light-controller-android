c96583f34352e5734d44de3fb8b55568905a84f1
Mafe <ladrillito966@gmail.com>
2025-10-22 19:41:08 +0200

    feat(ui): add basic intent navigation and initial layout for MainActivity2

--- app/src/main/java/com/empresa/domoticon/MainActivity.kt
+++ app/src/main/java/com/empresa/domoticon/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.empresa.domoticon
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +20,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val intent2 = Intent(this, MainActivity2::class.java)
+        val intent3 = Intent(this, MainActivity2::class.java)
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p: AdapterView<*>, v: View?, pos: Int, id: Long) {
+                if (pos == 1) {
+                    startActivity(intent2)
+                } else if (pos ==2){
+                    startActivity(intent3)
+                }
+
+            }
+            override fun onNothingSelected(p: AdapterView<*>) {}
+        }
 
 
     }
