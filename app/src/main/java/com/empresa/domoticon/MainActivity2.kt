e304fded6eee5e5b29e34498bab13cd62c3bd854
Mafe <ladrillito966@gmail.com>
2025-10-24 12:37:45 +0200

    refactor: remove duplicate functions to optimize code

--- app/src/main/java/com/empresa/domoticon/MainActivity2.kt
+++ app/src/main/java/com/empresa/domoticon/MainActivity2.kt
@@ -1,12 +1,14 @@
 package com.empresa.domoticon
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.empresa.domoticon.databinding.Activity3Binding
 import com.empresa.domoticon.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,23 +18,30 @@ class MainActivity2 : AppCompatActivity() {
             2 -> { // Si selecciono 2 bombillas, lo redirige al layout activity_main_2
                 val binding = ActivityMain2Binding.inflate(layoutInflater)
                 setContentView(binding.root)
-                encenderBombillas2(numBombillas, binding)
+
+                val bombillas = arrayOf(binding.img1, binding.img2,)
+                val botones = arrayOf(binding.btn1, binding.btn2)
+                val checkboxes = arrayOf(binding.checkbox1, binding.checkbox2)
+                val btnReset = binding.btnReset
+
+                logicaBotones(bombillas, botones, checkboxes, btnReset)
             }
 
             3 -> { // Si selecciono 3 bombillas, lo redirige al layout activity_3
                 val binding = Activity3Binding.inflate(layoutInflater)
                 setContentView(binding.root)
-                encenderBombillas3(numBombillas, binding)
-            }
 
+                val bombillas = arrayOf(binding.img1, binding.img2, binding.img3)
+                val botones = arrayOf(binding.btn1, binding.btn2, binding.btn3)
+                val checkboxes = arrayOf(binding.checkbox1, binding.checkbox2, binding.checkbox3)
+                val btnReset = binding.btnReset
+
+                logicaBotones(bombillas, botones, checkboxes, btnReset)
+            }
         }
     }
 
-    fun encenderBombillas2(numBombillas: Int, binding: ActivityMain2Binding) {
-        val bombillas = arrayOf(binding.img1, binding.img2,)
-        val botones = arrayOf(binding.btn1, binding.btn2)
-        val checkboxes = arrayOf(binding.checkbox1, binding.checkbox2)
-
+    private fun logicaBotones(bombillas: Array<ImageView>, botones: Array<Button>, checkboxes: Array<CheckBox>, btnReset: Button) {
         botones.forEachIndexed { i, boton ->
             boton.setOnClickListener {
                 if (boton.text == "OFF") {
@@ -45,7 +54,7 @@ class MainActivity2 : AppCompatActivity() {
             }
         }
 
-        binding.btnReset.setOnClickListener {
+        btnReset.setOnClickListener {
             checkboxes.forEachIndexed { i, checkBox ->
                 if (checkBox.isChecked) {
                     botones[i].text = "OFF"
@@ -54,33 +63,4 @@ class MainActivity2 : AppCompatActivity() {
             }
         }
     }
-
-    fun encenderBombillas3(numBombillas: Int, binding: Activity3Binding) {
-        val bombillas = arrayOf(binding.img1, binding.img2, binding.img3)
-        val botones = arrayOf(binding.btn1, binding.btn2, binding.btn3)
-        val checkboxes = arrayOf(binding.checkbox1, binding.checkbox2, binding.checkbox3)
-
-        botones.forEachIndexed { i, boton ->
-            boton.setOnClickListener {
-                if (boton.text == "OFF") {
-                    boton.text = "ON"
-                    bombillas[i].setImageResource(R.drawable.bulb_on)
-                } else {
-                    boton.text = "OFF"
-                    bombillas[i].setImageResource(R.drawable.bulb_off)
-                }
-            }
-        }
-
-        binding.btnReset.setOnClickListener {
-            checkboxes.forEachIndexed { i, checkBox ->
-                if (checkBox.isChecked) {
-                    botones[i].text = "OFF"
-                    bombillas[i].setImageResource(R.drawable.bulb_off)
-                }
-            }
-        }
-
-    }
-
 }
